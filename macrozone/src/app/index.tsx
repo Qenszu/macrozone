147f94e0d028b6063664f811aba6851f436e4ed3
Qenszu <Opalusaktv@gmail.com>
2026-05-05 00:12:05 +0200

    Working on styles and home page

---
+++ macrozone/src/app/index.tsx
@@ -0,0 +1,20 @@
+import { globalStyles } from "@/styles/global";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View style={globalStyles.container}>
+      <Text style={globalStyles.title}>MacroZone</Text>
+      <Text style={styles.date}>Monday, March 16</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  date: {
+    fontSize: 14,
+    color: "#a0a0b0",
+    marginTop: 4,
+    marginBottom: 30,
+  },
+});
